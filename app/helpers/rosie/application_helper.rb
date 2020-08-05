90a71301d9d3afa676f9c332c28f61f496a4001c
Anton Baronov <anton.baronov@gmail.com>
2020-08-05 23:19:11 +0300

    Added mounted_path url helper and fixed paths for non root Rosie mounts

--- app/helpers/rosie/application_helper.rb
+++ app/helpers/rosie/application_helper.rb
@@ -4,5 +4,12 @@ module Rosie
       time_ago_in_words(time).gsub('less than a minute', '0m').gsub('about ', '').gsub(/ hour[s]?/, 'h').
         gsub(/ minute[s]?/, 'm').gsub(/ day[s]?/, 'd').gsub(/ month[s]?/, 'm') + ' ago'
     end
+    def mounted_path relative_path
+      relative_path = relative_path[1..-1] if relative_path.start_with?('/')
+      mounted_path  = MemoryStore.fetch_invalidate('yay', true) do
+        (rosie.routes.find_script_name({})+'/').gsub('//','/')
+      end
+      mounted_path  + relative_path
+    end
   end
 end
